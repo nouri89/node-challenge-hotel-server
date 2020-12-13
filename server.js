e9b9bf361263c9d42080787c032d9b815608d0da
nouri <60990203+nouri89@users.noreply.github.com>
2020-12-13 20:14:31 +0000

    make it better

--- server.js
+++ server.js
@@ -1,20 +1,76 @@
 const express = require("express");
 const cors = require("cors");
-
+const moment = require("moment");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 //Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
+let bookings = require("./bookings.json");
+//console.log(bookings);
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+app.get("/bookings", function (request, response) {
+	response.json(bookings);
+});
+app.post("/bookings", function (request, response) {
+	let newBooking = request.body;
+	console.log(typeof newBooking.roomId);
+	if (
+		newBooking.title.length == 0 ||
+		newBooking.firstName.length == 0 ||
+		newBooking.surname.length == 0 ||
+		newBooking.email.length == 0 ||
+		newBooking.roomId == "" ||
+		newBooking.title.length == "" ||
+		newBooking.title.length == ""
+	) {
+		response.sendStatus(400);
+	} else {
+		newBooking.id = bookings.length + 1;
+		bookings.push(newBooking);
+		response.json("booking added");
+	}
 });
 
-// TODO add your routes and helper functions here
+app.get("/bookings/search", function (request, response) {
+	let searchedDate = request.query.date;
+	let convertedDate = moment([searchedDate]);
+	console.log(searchedDate);
+	response.json(convertedDate);
+});
+app.get("/bookings/:id", function (request, response) {
+	let bookingId = request.params.id;
+	console.log(typeof bookingId);
+	console.log(bookingId);
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+	let foundBooking = bookings.find((booking) => booking.id == bookingId);
+	if (foundBooking) {
+		response.json(foundBooking);
+	} else {
+		response.sendStatus(404);
+	}
+});
+
+app.delete("/bookings/:id", function (request, response) {
+	let bookingId = request.params.id;
+	let foundBooking = bookings.find((booking) => booking.id == bookingId);
+	if (!foundBooking) {
+		response.sendStatus(404);
+	} else {
+		bookings = bookings.filter((b) => b.id != bookingId);
+		response.json("booking deleted");
+	}
+});
+
+let port = process.env.PORT || 4000;
+const listener = app.listen(port, function () {
+	console.log("Your app is  listening on port " + port);
 });
